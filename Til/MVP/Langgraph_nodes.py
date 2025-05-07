b5639a01e130696263cfe44dc5b1b368998159c5
moosunny <a01088415234@gmail.com>
2025-05-07 07:06:29 +0000

    feat: requirements.txt 파일 수정

--- Til/MVP/Langgraph_nodes.py
+++ Til/MVP/Langgraph_nodes.py
@@ -15,7 +15,7 @@ class Langgraph:
     def __init__(self, files_num, model: TILModels):
         self.prompts = LanggraphPrompts()
         self.model = model
-        self.client = QdrantClient(host="10.128.0.8", port=6333)
+        self.client = QdrantClient(host="34.44.240.237", port=6333)
         self.files_num = files_num
         self.graph = self._build_graph()
         
