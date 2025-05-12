540f96ed3f2cf9157ac1ca5434277e6cb3d4074b
moosunny <a01088415234@gmail.com>
2025-05-12 10:05:16 +0000

    update: Til 생성 Discord에 전달 코드 추가

--- Til/MVP/main.py
+++ Til/MVP/main.py
@@ -7,14 +7,25 @@ from model import *
 from dotenv import load_dotenv
 import uvicorn
 import nest_asyncio
+import os
+import httpx
 
 # 디버깅 패키지
 import traceback
+
+DISCORD_WEBHOOK_URL = os.getenv("DISCORD_WEBHOOK_URL")
 
 load_dotenv()
 app = FastAPI(debug=True)
 # 프로메테우스 연동
 Instrumentator().instrument(app).expose(app)
+
+async def send_discord_notification(content: str):
+    if not DISCORD_WEBHOOK_URL:
+        raise ValueError("DISCORD_WEBHOOK_URL is not set in .env")
+
+    async with httpx.AsyncClient() as client:
+        await client.post(DISCORD_WEBHOOK_URL, json={"content": content})
 
 model = get_til_model()
 
@@ -31,6 +42,8 @@ async def process_til(data: StateModel):
         # vector만 제외하고 dict로 반환
         til_json = result["til_json"]
         til_json_dict = til_json.dict(exclude={"vector"})
+        
+        await send_discord_notification(til_json_dict["content"])
         
         return til_json_dict
         
