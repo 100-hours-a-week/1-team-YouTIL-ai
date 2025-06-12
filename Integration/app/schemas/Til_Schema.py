cd5ecdf054ae1f56c610097638c6b8e98d989434
moosunny <a01088415234@gmail.com>
2025-06-12 11:32:13 +0000

    fix: TIlKeywords 스키마 에러 해결

--- Integration/app/schemas/Til_Schema.py
+++ Integration/app/schemas/Til_Schema.py
@@ -63,7 +63,7 @@ class StateModel(BaseModel):
     username: str
     date: str
     repo: str
-    files: List[FileModel]  # node_id가 포함된 파일 리스트
+    files: List[FileModel] 
 
     # 선택 필드들 (초기엔 없을 수 있음)
     code_summary: Annotated[Dict[str, str], merge_dicts] = Field(default_factory=dict)
