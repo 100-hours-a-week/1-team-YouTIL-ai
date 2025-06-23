228abcb3ee72e69acdf00dad5f53551613d041a3
moosunny <a01088415234@gmail.com>
2025-06-23 07:12:30 +0000

    fix: fastapi workers 5 설정 추가

--- Integration/main.py
+++ Integration/main.py
@@ -7,8 +7,8 @@ app.include_router(router)
 
 Instrumentator().instrument(app).expose(app)
 
-if __name__ == "__main__":
-    import uvicorn
-    import nest_asyncio
-    nest_asyncio.apply()
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+# if __name__ == "__main__":
+#     import uvicorn
+#     import nest_asyncio
+#     nest_asyncio.apply()
+#     uvicorn.run(app, host="0.0.0.0", port=8000)
